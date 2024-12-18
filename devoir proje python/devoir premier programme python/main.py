8593082b0eca18585a1c7b272b6a901d916450ce
junior733-j <saintlotjunior330@gmail.com>
2024-12-18 16:45:03 -0500

    mw ta renmen kelkeswa moun nan korije sa pou mw
    merci ak ou ki ap gade kod mw yo

---
+++ devoir proje python/devoir premier programme python/main.py
@@ -0,0 +1,39 @@
+
+quantite_pepsi = 130
+quantite_prix_unitaire_du_pepsi =1.50
+revenu_pepsi = quantite_pepsi * quantite_prix_unitaire_du_pepsi
+print(revenu_pepsi)
+revenu_pepsi = 195.0 
+
+
+quantite_coca_cola = 150
+quantie_prix_unitaire_du_coca = 1.50
+revenu_coca = quantite_coca_cola * quantie_prix_unitaire_du_coca
+print(revenu_coca)
+revenu_coca = 225.0
+
+
+revenu_pepsi = 195.0
+revenu_coca  = 225.0
+revenu_total = revenu_pepsi + revenu_coca
+print(revenu_total)
+revenu_total = 420.0
+
+
+
+revenu_total = 420.0
+depense_magasin =2500
+benefice = revenu_total - depense_magasin
+print(benefice)
+benefice = -2080.0
+
+
+benefice = -2080.0
+revenu_total = 420.0
+marge = benefice / revenu_total
+print(marge)
+marge = -4.9523809523809526
+
+
+
+
